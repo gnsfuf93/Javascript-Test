233b590e3c87bcc0d56a1f54fd9e6a826b0c29f9
gnsfuf93 <gnsfuf93@gmail.com>
2021-08-11 10:26:45 +0900

    Javascript Test Project

---
+++ javascript-module/moduleTest2-3.js
@@ -0,0 +1,10 @@
+var word = "Good Morning";
+
+function test1() {
+  console.log("test1 > word", word);
+}
+function test2() {
+  console.log("test2 > word", word);
+}
+
+export default { word, test1, test2 };
